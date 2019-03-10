9f4a85556603dbc9d5c3778e9a2f7e4bd916ac43
chenshuai <384619043@qq.com>
2019-03-10 15:12:08 +0800

    immutable.js的使用thunk管理ajax请求

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { CSSTransition } from "react-transition-group";
 import {
   HeaderWrapper,
@@ -8,63 +8,93 @@ import {
   NavSearch,
   Addition,
   Button,
-  SearchWrapper
+  SearchWrapper,
+  SearchInfo,
+  SearchInfoTitle,
+  SearchInfoSwitch,
+  SearchInfoItem,
+  SearchInfoList
 } from "./style";
 import { connect } from "react-redux";
+import { actionCreators } from "./store";
 
-const Header = props => {
-  return (
-    <HeaderWrapper>
-      <Logo />
-      <Nav>
-        <NavItem className="left active">首页</NavItem>
-        <NavItem className="left">下载App</NavItem>
-        <NavItem className="right">登录</NavItem>
-        <NavItem className="right">
-          <span className="iconfont">&#xe636;</span>
-        </NavItem>
-        <SearchWrapper>
-          <CSSTransition timeout={200} in={props.focused} classNames="slide">
-            <NavSearch
-              className={props.focused ? "focused" : ""}
-              onFocus={props.handleInputFocus}
-              onBlur={props.handleInputBlur}
-            />
-          </CSSTransition>
-          <span className={props.focused ? "focused iconfont" : "iconfont"}>
-            &#xe62d;
-          </span>
-        </SearchWrapper>
-      </Nav>
-      <Addition>
-        <Button className="writing">
-          <span className="iconfont">&#xe615;</span>
-          写文章
-        </Button>
-        <Button className="reg">注册</Button>
-      </Addition>
-    </HeaderWrapper>
-  );
-};
+class Header extends Component {
+  getListArea() {
+    if (this.props.focused) {
+      return (
+        <SearchInfo>
+          <SearchInfoTitle>
+            热门搜索
+            <SearchInfoSwitch>换一批</SearchInfoSwitch>
+          </SearchInfoTitle>
+          <SearchInfoList>
+            {this.props.list.map(item => {
+              return <SearchInfoItem key={item}>{item}</SearchInfoItem>;
+            })}
+          </SearchInfoList>
+        </SearchInfo>
+      );
+    } else {
+      return null;
+    }
+  }
+  render() {
+    return (
+      <HeaderWrapper>
+        <Logo />
+        <Nav>
+          <NavItem className="left active">首页</NavItem>
+          <NavItem className="left">下载App</NavItem>
+          <NavItem className="right">登录</NavItem>
+          <NavItem className="right">
+            <span className="iconfont">&#xe636;</span>
+          </NavItem>
+          <SearchWrapper>
+            <CSSTransition
+              timeout={200}
+              in={this.props.focused}
+              classNames="slide"
+            >
+              <NavSearch
+                className={this.props.focused ? "focused" : ""}
+                onFocus={this.props.handleInputFocus}
+                onBlur={this.props.handleInputBlur}
+              />
+            </CSSTransition>
+            <span
+              className={this.props.focused ? "focused iconfont" : "iconfont"}
+            >
+              &#xe62d;
+            </span>
+            {this.getListArea()}
+          </SearchWrapper>
+        </Nav>
+        <Addition>
+          <Button className="writing">
+            <span className="iconfont">&#xe615;</span>
+            写文章
+          </Button>
+          <Button className="reg">注册</Button>
+        </Addition>
+      </HeaderWrapper>
+    );
+  }
+}
 
 const mapStateToProps = state => {
   return {
-    focused: state.header.focused
+    focused: state.getIn(["header", "focused"]),
+    list: state.getIn(["header", "list"])
   };
 };
 const mapDispatchToProps = dispatch => {
   return {
     handleInputFocus() {
-      const action = {
-        type: "search_focus"
-      };
-      dispatch(action);
+      dispatch(actionCreators.getList());
+      dispatch(actionCreators.searchFocus());
     },
     handleInputBlur() {
-      const action = {
-        type: "search_blur"
-      };
-      dispatch(action);
+      dispatch(actionCreators.searchBlur());
     }
   };
 };
